6027406cf22e715a36ed55a2b4d769a119308559
pkornijasz <pk@nkk.pl>
2021-06-28 01:38:32 +0200

    V2

--- src/main/java/pl/kornijasz/JsoupTest.java
+++ src/main/java/pl/kornijasz/JsoupTest.java
@@ -10,13 +10,16 @@ public class JsoupTest {
 
     public static void main(String[] args) throws IOException {
 
-        final Document page = Jsoup.connect("https://www.benq.eu/pl-pl/projector/cineprime-home-cinema/w2700/specifications.html").get();
+        final Document page = Jsoup.connect("https://www.benq.eu/pl-pl/projector/home-entertainment/x1300i/specifications.html").get();
 
+
+        System.out.println();
+        System.out.println("Klucz -> Wartość");
+        System.out.println();
         for (Element searchResult : page.select("div.row.data_row.f_gray")) {
             final String key = searchResult.getElementsByClass("col-md-4 col-sm-6 col-xs-6").text();
             final String value = searchResult.getElementsByClass("col-md-8 col-sm-6 col-xs-6").text();
             System.out.println(key + " -> " + value);
-
         }
     }
 }
